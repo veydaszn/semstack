33cad70b6601d4ae0d128516c0cce300b2cf97e3
Veydaszn <mvngone4l@gmail.com>
2026-01-22 12:41:07 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,6 @@
+class StudyMaterial:
+    def __init__(self, semester, course, topic, content):
+        self.semester = semester
+        self.course = course
+        self.topic = topic
+        self.content = content
